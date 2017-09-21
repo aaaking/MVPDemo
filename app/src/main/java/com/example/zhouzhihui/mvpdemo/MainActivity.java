eb46f493824cbe77a317ef40651a1e92f7a68e13
zhouzhihui <zhouzhihui@htgames.cn>
2017-09-21 16:13:08 +0800

    do nothing

--- app/src/main/java/com/example/zhouzhihui/mvpdemo/MainActivity.java
+++ app/src/main/java/com/example/zhouzhihui/mvpdemo/MainActivity.java
@@ -68,7 +68,6 @@ public class MainActivity extends AppCompatActivity
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
-            return true;
         }
 
         return super.onOptionsItemSelected(item);
